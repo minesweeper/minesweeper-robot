48cc2ba47b4d14ca149e96cc776eb42a4c770bf5
Mark Ryall <mark.ryall@gmail.com>
2012-01-09 16:41:53 +1000

    some obssessive cleaning up of spec

--- spec/minesweeper/robot_spec.rb
+++ spec/minesweeper/robot_spec.rb
@@ -10,11 +10,19 @@ describe Minesweeper::Robot do
     game.should_receive(:field).and_return Minesweeper::Game.string_to_field field_string
   end
 
-  it 'should detect obvious mines' do
+  it 'should detect obvious mines with 1 adjacent' do
     field <<-EOF
     1 1 .
     1 . .
     EOF
     robot.obvious_mines.should == [[1, 1]]
+  end
+
+  it 'should detect obvious mines with 2 adjacent' do
+    field <<-EOF
+    2 1 .
+    . . .
+    EOF
+    robot.obvious_mines.should == [[0,1],[1, 1]]
   end
 end
