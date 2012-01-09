4ae43d25b93203d4da2eb9677688cb868479b4b1
Mark Ryall <mark.ryall@gmail.com>
2012-01-09 13:38:34 +1000

    introduced the same amazing dsl for setting out the minefield in a test

--- spec/minesweeper_robot_spec.rb
+++ spec/minesweeper_robot_spec.rb
@@ -14,12 +14,20 @@ describe Minesweeper::Robot do
     game.destroy
   end
 
-  def with_game options
-    game.reset options
+  def with_game field_string
+    lines = field_string.split("\n").map &:split
+    mines = []
+    lines.each_with_index do |row, rindex|
+      row.each_with_index { |col, cindex| mines << [rindex, cindex] if col == '*' }
+    end
+    game.reset rows: lines.length, cols: lines.map(&:length).max, mines: mines, mineCount: mines.length
   end
 
   it 'should get initial field containing only unknown status' do
-    with_game rows: 2, cols: 2
+    with_game <<-EOF
+    . .
+    . .
+    EOF
     game.field.should == [
       [:unclicked, :unclicked],
       [:unclicked, :unclicked]
@@ -27,7 +35,9 @@ describe Minesweeper::Robot do
   end
 
   it 'should win the game if the mine is clicked' do
-    with_game rows: 1, cols: 2, mineCount: 1
+    with_game <<-EOF
+    . *
+    EOF
     game.click 0, 0
     game.field.should == [
       [:mines1, :unclicked]
