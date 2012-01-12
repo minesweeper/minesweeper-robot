e3f9dc6c54e023e9c09449c930f5c0059711a972
Mark Ryall <mark.ryall@gmail.com>
2012-01-12 11:22:50 +1000

    switched to using expert mode in robot

--- lib/minesweeper/robot.rb
+++ lib/minesweeper/robot.rb
@@ -20,7 +20,7 @@ class Minesweeper::Robot
     beginner =     {rows: 9,  cols: 9,  mineCount: 10}
     intermediate = {rows: 16, cols: 16, mineCount: 40}
     expert =       {rows: 16, cols: 30, mineCount: 99}
-    @options = intermediate.merge options
+    @options = expert.merge options
     rows,cols,mineCount = *%w{rows cols mineCount}.map {|key| @options[key.to_sym] }
     won = 0
     lost = 0
